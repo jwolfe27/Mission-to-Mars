96f9a8842ae1cf81fcaeff3d053d7b9bc3bede3d
Jake Wolfe <89044350+jwolfe27@users.noreply.github.com>
2021-10-27 17:41:21 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_pymongo import Pymongo
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
 import scraping
 
 app = Flask(__name__)
